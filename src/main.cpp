771e23b5fd8cf8fc24919ff371583ad9481d5784
Olivier Vincent <o.vincent@polymtl.ca>
2018-08-24 18:13:01 -0400

    fixed bug

--- src/main.cpp
+++ src/main.cpp
@@ -10,10 +10,12 @@ int main(){
 	//pulseOxymeter = new MAX30100( DEFAULT_OPERATING_MODE, DEFAULT_SAMPLING_RATE, DEFAULT_LED_PULSE_WIDTH, DEFAULT_IR_LED_CURRENT, true, true );
 	pulseOxymeter = new MAX30100();
 
+	printf("test1 \n");
 	//You have to call update with frequency at least 37Hz. But the closer you call it to 100Hz the better, the filter will work.
   pulseoxymeter_t result = pulseOxymeter->update();
   
-
+	printf("test2 \n");
+  
   if( result.pulseDetected == true )
   {
     printf("BEAT \n");
@@ -26,5 +28,7 @@ int main(){
     printf("%u", result.SaO2 );
     printf( "% \n" );
   }
+  
+	printf("test3 \n");
 return 0;	
 }
