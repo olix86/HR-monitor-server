0a24bcedab137326abb5ec00dc6432ec8b47e703
Olivier Vincent <o.vincent@polymtl.ca>
2018-08-25 10:55:21 -0400

    fixed bug

--- src/MAX30100.cpp
+++ src/MAX30100.cpp
@@ -321,7 +321,7 @@ void MAX30100::writeRegister(byte address, byte val)
 	}
 	else
 	{
-		printf("ok, value = %u",val);
+		//printf("ok, value = %u",val);
 		
 	}
 }
@@ -349,15 +349,8 @@ uint8_t MAX30100::readRegister(uint8_t address)
 		return -1;
 	}
 	
-	if (write(file_i2c, &address, 1) != 1)		//write() returns the number of bytes actually written, if it doesn't match then an error occurred (e.g. no response from the device)
-	{
-		/* ERROR HANDLING: i2c transaction failed */
-		printf("Failed to write to the i2c bus.\n");
-	}
-	
-	//----- READ BYTES -----
-	//length = 1;			//<<< Number of bytes to read
-	if (read(file_i2c, &buffer, 1) != 1)		//read() returns the number of bytes actually read, if it doesn't match then an error occurred (e.g. no response from the device)
+	int result = i2c_smbus_read_byte_data(file_i2c, address);
+	if (result < 0)		//read() returns the number of bytes actually read, if it doesn't match then an error occurred (e.g. no response from the device)
 	{
 		//ERROR HANDLING: i2c transaction failed
 		printf("Failed to read from the i2c bus.\n");
@@ -366,6 +359,7 @@ uint8_t MAX30100::readRegister(uint8_t address)
 	{
 		printf("Data read: %u \n", buffer);
 	}
+	buffer = result;
 	return buffer;
 }
 
