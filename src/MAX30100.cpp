22b0f5c354b0f000bb79636abcffe053df5a9fe5
Olivier Vincent <o.vincent@polymtl.ca>
2018-08-25 10:18:51 -0400

    fixed bug

--- src/MAX30100.cpp
+++ src/MAX30100.cpp
@@ -304,6 +304,12 @@ void MAX30100::writeRegister(byte address, byte val)
 	//----- WRITE BYTES -----
 	//buffer[0] = val;
 	length = 1;			//<<< Number of bytes to write
+	if (write(file_i2c, &address, 1) != 1)		//write() returns the number of bytes actually written, if it doesn't match then an error occurred (e.g. no response from the device)
+	{
+		/* ERROR HANDLING: i2c transaction failed */
+		printf("Failed to write to the i2c bus.\n");
+	}
+	
 	if (write(file_i2c, &val, length) != length)		//write() returns the number of bytes actually written, if it doesn't match then an error occurred (e.g. no response from the device)
 	{
 		/* ERROR HANDLING: i2c transaction failed */
@@ -334,6 +340,11 @@ uint8_t MAX30100::readRegister(uint8_t address)
 		return -1;
 	}
 	
+	if (write(file_i2c, &address, 1) != 1)		//write() returns the number of bytes actually written, if it doesn't match then an error occurred (e.g. no response from the device)
+	{
+		/* ERROR HANDLING: i2c transaction failed */
+		printf("Failed to write to the i2c bus.\n");
+	}
 	
 	//----- READ BYTES -----
 	//length = 1;			//<<< Number of bytes to read
@@ -372,7 +383,11 @@ void MAX30100::readFrom(byte address, int length, byte _buff[])
 		return;
 	}
 	
-	
+	if (write(file_i2c, &address, 1) != 1)		//write() returns the number of bytes actually written, if it doesn't match then an error occurred (e.g. no response from the device)
+	{
+		/* ERROR HANDLING: i2c transaction failed */
+		printf("Failed to write to the i2c bus.\n");
+	}
 	//----- READ BYTES -----
 	//length = num;			//<<< Number of bytes to read
 	if (read(file_i2c, _buff, length) != length)		//read() returns the number of bytes actually read, if it doesn't match then an error occurred (e.g. no response from the device)
