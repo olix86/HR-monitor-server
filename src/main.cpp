e3b268c9570995d405d0d820c817a204113a34ca
Olivier Vincent <o.vincent@polymtl.ca>
2018-09-09 17:48:42 -0400

    added files for MPU6050 accelero

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,35 @@
-#include "MAX30100.h"
+#include "main.h" 
+// class default I2C address is 0x68
+// specific I2C addresses may be passed as a parameter here
+// AD0 low = 0x68 (default for InvenSense evaluation board)
+// AD0 high = 0x69
+MPU6050 accelgyro;
+
+int16_t ax, ay, az;
+int16_t gx, gy, gz;
+
+void setupAccelero() {
+    // initialize device
+    printf("Initializing I2C devices...\n");
+    accelgyro.initialize();
+
+    // verify connection
+    printf("Testing device connections...\n");
+    printf(accelgyro.testConnection() ? "MPU6050 connection successful\n" : "MPU6050 connection failed\n");
+}
+
+void readAccelero() {
+    // read raw accel/gyro measurements from device
+    accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+
+    // these methods (and a few others) are also available
+    //accelgyro.getAcceleration(&ax, &ay, &az);
+    //accelgyro.getRotation(&gx, &gy, &gz);
+
+    // display accel/gyro x/y/z values
+    printf("a/g: %6hd %6hd %6hd   %6hd %6hd %6hd\n",ax,ay,az,gx,gy,gz);
+}
+
 
 int main(){
 	
