d274c05fb94b65c06b9bce95e4c5d930a975960f
Olivier Vincent <o.vincent@polymtl.ca>
2018-09-10 14:09:48 -0400

    bug fix

--- src/main.cpp
+++ src/main.cpp
@@ -7,30 +7,121 @@ MPU6050 accelgyro;
 
 int16_t ax, ay, az;
 int16_t gx, gy, gz;
+enum States (wait, freefall, impact, rest);
 
 void setupAccelero() {
-    // initialize device
-    printf("Initializing I2C devices...\n");
-    accelgyro.initialize();
-
-    // verify connection
-    printf("Testing device connections...\n");
-    printf(accelgyro.testConnection() ? "MPU6050 connection successful\n" : "MPU6050 connection failed\n");
+	// initialize device
+	printf("Initializing I2C devices...\n");
+	accelgyro.initialize();
+	
+	// verify connection
+	printf("Testing device connections...\n");
+	printf(accelgyro.testConnection() ? "MPU6050 connection successful\n" : "MPU6050 connection failed\n");
 }
 
 void readAccelero() {
-    // read raw accel/gyro measurements from device
-    accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
-
-    // these methods (and a few others) are also available
-    //accelgyro.getAcceleration(&ax, &ay, &az);
-    //accelgyro.getRotation(&gx, &gy, &gz);
-
-    // display accel/gyro x/y/z values
+	// read raw accel/gyro measurements from device
+	accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+	
+	// these methods (and a few others) are also available
+	//accelgyro.getAcceleration(&ax, &ay, &az);
+	//accelgyro.getRotation(&gx, &gy, &gz);
+	
+	// display accel/gyro x/y/z values
 	double scaling_factor = 16384;
-    //printf("a/g: %6hd %6hd %6hd   %6hd %6hd %6hd\n",(double)ax,(double)ay,(double)az,gx,gy,gz);
+	//printf("a/g: %6hd %6hd %6hd   %6hd %6hd %6hd\n",(double)ax,(double)ay,(double)az,gx,gy,gz);
 	printf("a/g: %f %f %f   %6hd %6hd %6hd\n",(double)ax/scaling_factor,(double)ay/scaling_factor,(double)az/scaling_factor,gx,gy,gz);
 }
+
+States fsm()
+{
+	static float g=9.81;
+	static float T = 0.01;
+	static float maxFallTime = 1.0;
+	static float maxImpactTime = 1.0;
+	static float maxRestTime = 1.0;
+	static States state = wait;
+	
+	static float waitImpact = 0;
+	static float waitRest = 0;
+	static float restCounter = 0;
+	// read raw accel/gyro measurements from device
+	accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+	
+	// these methods (and a few others) are also available
+	//accelgyro.getAcceleration(&ax, &ay, &az);
+	//accelgyro.getRotation(&gx, &gy, &gz);
+	
+	// display accel/gyro x/y/z values
+	double scaling_factor = 16384;
+	//printf("a/g: %6hd %6hd %6hd   %6hd %6hd %6hd\n",(double)ax,(double)ay,(double)az,gx,gy,gz);
+	printf("a/g: %f %f %f   %6hd %6hd %6hd\n",(double)ax/scaling_factor,(double)ay/scaling_factor,(double)az/scaling_factor,gx,gy,gz);
+	printf("%d \n", state);
+	
+	if (state == wait)
+	{	if(norm <(0.5*g))
+		{
+			state = states.freefall
+		}
+		else
+		{
+			state = states.wait
+		}
+	}
+	else if(state == freefall)
+	{	
+		if(norm >(3.0*g))
+		{
+			state = states.impact
+			waitImpact = 0
+		}
+		else if(waitImpact > maxFallTime/T)
+		{
+			state = states.wait
+			waitImpact = 0
+		}
+		else:
+		{
+			waitImpact = waitImpact + 1
+			state = freefall
+		}
+	}
+	else if(state == impact)
+	{
+		if(norm < (1.5*g))
+		{
+			state = states.rest
+			waitRest = 0
+		}
+		else if(waitRest > maxImpactTime/T)
+		{
+			state = states.wait
+			waitRest = 0
+		}
+		else
+		{
+			waitRest = waitRest + 1
+			state = states.impact
+		}
+	}
+	//State rest
+	else
+	{
+		if(restCounter > maxRestTime/T)
+		{
+			state = wait
+			restCounter = 0
+		}
+		else
+		{
+			state = states.rest
+			restCounter = restCounter + 1
+		}
+	}
+	
+}
+
+
 
 
 int main(){
@@ -59,7 +150,7 @@ int main(){
 			printf("Error opening file!\n");
 			exit(1);
 		}
-			
+		
 		if( result.pulseDetected == true )
 		{
 			//printf("BEAT \n");
@@ -82,7 +173,8 @@ int main(){
 		
 		fclose(f);
 		
-		readAccelero();
+		//readAccelero();
+		fsm();
 		//takes microseconds as arg
 		usleep(10*1000);
 		//i--;
